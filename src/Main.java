87a0b872133cb12049f93a045989fffc536b910f
tweak22 <arsencik141271@gmail.com>
2025-12-16 17:33:03 +0200

    Feat: Add miles to kilometers conversion logic

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Проект homework2pro. Версія 1.0");
-        System.out.println("Автор: Арсеній");
+        System.out.println("Проект Converter. Версія 1.1");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введіть кількість миль:");
+        double miles = scanner.nextDouble();
+        double kilometers = miles * 1.60934;
+
+        System.out.println(miles + " миль = " + kilometers + " кілометрів.");
     }
 }
