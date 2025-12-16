a40552515aa7db79e5c9497488c97f5384d09f8a
tweak22 <arsencik141271@gmail.com>
2025-12-16 17:24:59 +0200

    Initial commit: Project setup and info output

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Проект homework2pro. Версія 1.0");
+        System.out.println("Автор: Арсеній");
+    }
+}
